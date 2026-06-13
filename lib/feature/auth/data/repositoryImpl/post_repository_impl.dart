5c725de22b5e154d0644e59eb4e6b94d11318b8c
Earbaj <earbajsaria3@gmail.com>
2026-06-13 12:37:10 +0600

    ADD:post repository implementation

---
+++ lib/feature/auth/data/repositoryImpl/post_repository_impl.dart
@@ -0,0 +1,24 @@
+import 'package:dartz/dartz.dart';
+
+import '../../../../core/error/failure.dart';
+import '../../domain/entity/post_response_entity.dart';
+import '../../domain/repository/post_repository.dart';
+import '../datasource/post_remote_data_source.dart';
+import '../mapper/post_mapper.dart';
+
+
+class PostRepositoryImpl implements PostRepository {
+  final PostRemoteDataSource remoteDataSource;
+  const PostRepositoryImpl(this.remoteDataSource);
+
+  @override
+  Future<Either<Failure, PostResponseEntity>> getPosts({required int limit, required int skip}) async {
+    try {
+      final remoteData = await remoteDataSource.fetchPosts(limit: limit, skip: skip);
+      final entity = PostMapper.toEntity(remoteData);
+      return Right(entity);
+    } catch (e) {
+      return Left(ServerFailure(e.toString()));
+    }
+  }
+}
